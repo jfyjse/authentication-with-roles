a2ddd98bb96a649035cf27228d38ea518e5d9d17
Joffy Jose <joffy@sayonetech.com>
2024-04-23 10:53:50 +0530

    initial project setup

---
+++ authentication-with-roles/src/main/java/com/authentication/authentication/with/roles/AuthenticationWithRolesApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.authentication.with.roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationWithRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationWithRolesApplication.class, args);
+	}
+
+}
---
+++ authentication-with-roles/src/test/java/com/authentication/authentication/with/roles/AuthenticationWithRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.authentication.with.roles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationWithRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

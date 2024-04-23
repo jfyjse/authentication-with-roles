231c53c48f4b3a1835f1234b46482ef9332a0b9a
Joffy Jose <joffy@sayonetech.com>
2024-04-23 12:04:41 +0530

    controller updated

--- authentication-with-roles/src/main/java/com/authentication/authentication/with/roles/controllers/AdminController.java
+++ authentication-with-roles/src/main/java/com/authentication/authentication/with/roles/controllers/AdminController.java
@@ -1,4 +1,18 @@
 package com.authentication.authentication.with.roles.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin("*")
 public class AdminController {
+
+    @GetMapping("/")
+    public String sampleAdminController(){
+        return "Admin level access";
+    }
+
 }
--- authentication-with-roles/src/main/java/com/authentication/authentication/with/roles/controllers/AuthenticationController.java
+++ authentication-with-roles/src/main/java/com/authentication/authentication/with/roles/controllers/AuthenticationController.java
@@ -1,4 +1,27 @@
 package com.authentication.authentication.with.roles.controllers;
 
+import com.authentication.authentication.with.roles.dto.LoginResponseDTO;
+import com.authentication.authentication.with.roles.dto.RegistrationDTO;
+import com.authentication.authentication.with.roles.models.ApplicationUser;
+import com.authentication.authentication.with.roles.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ApplicationUser registerUser(@RequestBody RegistrationDTO body){
+        return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO loginUser(@RequestBody RegistrationDTO body){
+        return authenticationService.loginUser(body.getUsername(), body.getPassword());
+    }
 }
